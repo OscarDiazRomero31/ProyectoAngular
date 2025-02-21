c5ac60cfb0e6ff79eda87bc4ed236d5933a4c572
OscarDiazRomero31 <oscar.diaz.romero.al@iespoligonosur.org>
2025-02-22 00:40:24 +0100

    Navbar y Footer hechos, con todas las paginas.

--- angApp_v19-master/src/app/app.routes.ts
+++ angApp_v19-master/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FavoritosComponent } from './favoritos/favoritos.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'favoritos', component: FavoritosComponent},
+    {path: 'buscador', component: BuscadorComponent},
+    {path: 'contacto', component: ContactoComponent},
+  ];
+  
--- angApp_v19-master/src/app/home/home.component.ts
+++ angApp_v19-master/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CocktailService } from '../cocktail.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+  cocktails: any[] = [];
+  projectDescription: string = "Bienvenido a nuestro buscador de cócteles. Aquí puedes encontrar recetas, imágenes y todo lo que necesitas para disfrutar de deliciosos cócteles.";
+
+  constructor(private cocktailService: CocktailService) {}
+
+  ngOnInit(): void {
+    this.getRandomCocktailImages();
+  }
+
+  getRandomCocktailImages(): void {
+    this.cocktailService.getCocktailsByName("random").subscribe(data => {
+      this.cocktails = data.drinks; // Ajusta según la respuesta de tu API
+    });
+  }
 }
