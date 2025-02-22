33dff2b9ced7aeeb2ee3b6b2acd48beb85952a6f
OscarDiazRomero31 <oscar.diaz.romero.al@iespoligonosur.org>
2025-02-22 09:22:49 +0100

    Carrusel borrado

--- angApp_v19-master/src/app/cocktail.service.ts
+++ angApp_v19-master/src/app/cocktail.service.ts
@@ -1,17 +1,58 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'; // ✅ Aseguramos que map() esté importado
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root', // Esto hace que el servicio esté disponible en toda la aplicación
 })
 export class CocktailService {
-  private apiUrl: string = 'https://www.thecocktaildb.com/api/json/v1/1/';
+  private apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'; // URL base de la API
 
   constructor(private http: HttpClient) {}
 
-  // Método para obtener cócteles aleatorios
-  getRandomCocktails(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}random.php`);
+  // Obtener un cóctel aleatorio
+  getRandomCocktail(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + 'random.php');
+  }
+
+  // Buscar cócteles por nombre
+  searchCocktailByName(name: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}search.php?s=${name}`);
+  }
+
+  // Buscar cócteles por la primera letra
+  searchCocktailByFirstLetter(letter: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}search.php?f=${letter}`);
+  }
+
+  // Buscar cócteles por ingrediente
+  searchCocktailByIngredient(ingredient: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}filter.php?i=${ingredient}`);
+  }
+
+  // Obtener detalles completos de un cóctel por su ID
+  getCocktailById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}lookup.php?i=${id}`);
+  }
+
+  // Obtener categorías de cócteles
+  getCategories(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}list.php?c=list`);
+  }
+
+  // Obtener los cristales de cócteles
+  getGlasses(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}list.php?g=list`);
+  }
+
+  // Obtener ingredientes de cócteles
+  getIngredients(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}list.php?i=list`);
+  }
+
+  // Obtener cócteles alcohólicos o no alcohólicos
+  getCocktailsByAlcoholicType(type: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}filter.php?a=${type}`);
   }
 }
--- angApp_v19-master/src/app/home/home.component.ts
+++ angApp_v19-master/src/app/home/home.component.ts
@@ -1,30 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { CocktailService } from '../cocktail.service';
-import { CarrouselComponent } from '../carrousel/carrousel.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [CarrouselComponent],  // Importa CarrouselComponent
+  imports: [],  
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  cocktails: any[] = [];
+export class HomeComponent {
 
-  constructor(private cocktailService: CocktailService) {}
 
-  ngOnInit(): void {
-    this.getRandomCocktails();  // Llamamos al método para obtener cócteles aleatorios
-  }
-
-  getRandomCocktails() {
-    this.cocktailService.getRandomCocktails().subscribe({
-      next: (response) => {
-        this.cocktails = response.drinks.slice(0, 5); // Solo mostramos 5 cócteles para el carrusel
-      },
-      error: (error) => {
-        console.error('❌ Error al obtener cócteles:', error);
-      },
-    });
-  }
+  
 }
