ede1e3d833214db45dbbda6558fe4d56f0ba90b4
OscarDiazRomero31 <oscar.diaz.romero.al@iespoligonosur.org>
2025-02-24 02:06:58 +0100

    ultimo repaso

--- angApp_v19-master/src/app/random-images/random-images.component.ts
+++ angApp_v19-master/src/app/random-images/random-images.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./random-images.component.css']
 })
 export class RandomImagesComponent implements OnInit {
-  images: string[] = [];  // Array para almacenar las URLs de las imágenes
+  images: string[] = [];  
 
   constructor(private cocktailService: CocktailService) {}
 
@@ -17,20 +17,19 @@ export class RandomImagesComponent implements OnInit {
     this.loadRandomImages();
   }
 
-  // Función para obtener 4 imágenes aleatorias
+  
   loadRandomImages() {
-    this.images = [];  // Limpiar las imágenes antes de cargar nuevas
+    this.images = []; 
 
-    // Realizar 4 solicitudes para obtener imágenes aleatorias
+    
     for (let i = 0; i < 4; i++) {
       this.cocktailService.getRandomCocktails().subscribe((data: any) => {
-        this.images.push(data.drinks[0].strDrinkThumb);  // Almacenar la imagen
+        this.images.push(data.drinks[0].strDrinkThumb);  
       });
     }
   }
 
-  // Función que se ejecuta cuando el botón 'Recargar' es presionado
   reloadImages() {
-    this.loadRandomImages();  // Cargar nuevas imágenes aleatorias
+    this.loadRandomImages();  
   }
 }
