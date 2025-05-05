2fa429e48eccde8c5d9d86900cfac38a42f1e3ff
Oscar Diaz <oscar.diaz.romero.al@iespoligonosur.org>
2025-05-05 12:00:15 +0200

    App terminada, repasar y presentar.

--- angApp_v19-master/src/app/contacto/contacto.component.ts
+++ angApp_v19-master/src/app/contacto/contacto.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  contactoForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.contactoForm = this.fb.group({
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern('^[A-ZÁÉÍÓÚÑ][a-záéíóúñA-ZÁÉÍÓÚÑ ]*$')
+        ]
+      ],
+      correo: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactoForm.valid) {
+      const formData = this.contactoForm.value;
+      this.http.post('https://formspree.io/f/xnnjyzeq', formData).subscribe({
+        next: (response) => {
+          console.log('Formulario enviado correctamente', response);
+          this.contactoForm.reset();
+        },
+        error: (error) => {
+          console.error('Error al enviar el formulario', error);
+        }
+      });
+    } else {
+      this.contactoForm.markAllAsTouched();
+    }
+  }
 }
